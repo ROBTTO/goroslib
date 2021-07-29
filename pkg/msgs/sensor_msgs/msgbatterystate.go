776bf070e83cff051b0aac3492d93d174b8ee855
ROBTTO <731964305@qq.com>
2021-07-29 11:56:55 +0800

    update sensor_msgs battery state

--- pkg/msgs/sensor_msgs/msgbatterystate.go
+++ pkg/msgs/sensor_msgs/msgbatterystate.go
@@ -35,7 +35,6 @@ type BatteryState struct {
 	msg.Definitions       `ros:"uint8 POWER_SUPPLY_STATUS_UNKNOWN=0,uint8 POWER_SUPPLY_STATUS_CHARGING=1,uint8 POWER_SUPPLY_STATUS_DISCHARGING=2,uint8 POWER_SUPPLY_STATUS_NOT_CHARGING=3,uint8 POWER_SUPPLY_STATUS_FULL=4,uint8 POWER_SUPPLY_HEALTH_UNKNOWN=0,uint8 POWER_SUPPLY_HEALTH_GOOD=1,uint8 POWER_SUPPLY_HEALTH_OVERHEAT=2,uint8 POWER_SUPPLY_HEALTH_DEAD=3,uint8 POWER_SUPPLY_HEALTH_OVERVOLTAGE=4,uint8 POWER_SUPPLY_HEALTH_UNSPEC_FAILURE=5,uint8 POWER_SUPPLY_HEALTH_COLD=6,uint8 POWER_SUPPLY_HEALTH_WATCHDOG_TIMER_EXPIRE=7,uint8 POWER_SUPPLY_HEALTH_SAFETY_TIMER_EXPIRE=8,uint8 POWER_SUPPLY_TECHNOLOGY_UNKNOWN=0,uint8 POWER_SUPPLY_TECHNOLOGY_NIMH=1,uint8 POWER_SUPPLY_TECHNOLOGY_LION=2,uint8 POWER_SUPPLY_TECHNOLOGY_LIPO=3,uint8 POWER_SUPPLY_TECHNOLOGY_LIFE=4,uint8 POWER_SUPPLY_TECHNOLOGY_NICD=5,uint8 POWER_SUPPLY_TECHNOLOGY_LIMN=6"`
 	Header                std_msgs.Header
 	Voltage               float32
-	Temperature           float32
 	Current               float32
 	Charge                float32
 	Capacity              float32
@@ -46,7 +45,6 @@ type BatteryState struct {
 	PowerSupplyTechnology uint8
 	Present               bool
 	CellVoltage           []float32
-	CellTemperature       []float32
 	Location              string
 	SerialNumber          string
 }
